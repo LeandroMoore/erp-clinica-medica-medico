56ed146f83810a3089b849f53865f4570b4b264d
Roberto Sonnino <robertos@gmail.com>
2011-04-06 16:50:21 -0300

    Autenticação e nova tela Home (não-logado)

--- ERP.Medico/ERP.Medico/MainPage.xaml.cs
+++ ERP.Medico/ERP.Medico/MainPage.xaml.cs
@@ -1,14 +1,15 @@
-﻿namespace ERP.Medico
+﻿using System;
+
+namespace ERP.Medico
 {
     using System.Windows;
     using System.Windows.Controls;
     using System.Windows.Navigation;
-    using ERP.Medico.LoginUI;
 
     /// <summary>
     /// <see cref="UserControl"/> class providing the main UI for the application.
     /// </summary>
-    public partial class MainPage : UserControl
+    public partial class MainPage
     {
         /// <summary>
         /// Creates a new <see cref="MainPage"/> instance.
@@ -16,26 +17,51 @@
         public MainPage()
         {
             InitializeComponent();
+
+            VerificaAutenticacao();
+
+            var ctx = WebContext.Current;
+
+            ctx.Authentication.LoggedIn += (s, e) => DestravaAutenticacao();
+            ctx.Authentication.LoggedOut += (s, e) => TravaAutenticacao();
+
+        }
+
+        private void VerificaAutenticacao(bool estaNaHome = false)
+        {
+            var ctx = WebContext.Current;
+
+            if (!(ctx.User.IsAuthenticated))
+                TravaAutenticacao(estaNaHome);
+        }
+
+        private void TravaAutenticacao(bool estaNaHome = false)
+        {
+            LinksStackPanel.Visibility = Visibility.Collapsed;
+            if (!estaNaHome)
+                ContentFrame.Navigate(new Uri("/Home", UriKind.Relative));
+        }
+
+        private void DestravaAutenticacao()
+        {
+            LinksStackPanel.Visibility = Visibility.Visible;
+            ContentFrame.Navigate(new Uri("", UriKind.Relative));
         }
 
         /// <summary>
         /// After the Frame navigates, ensure the <see cref="HyperlinkButton"/> representing the current page is selected
         /// </summary>
-        private void ContentFrame_Navigated(object sender, NavigationEventArgs e)
+        private void ContentFrameNavigated(object sender, NavigationEventArgs e)
         {
             foreach (UIElement child in LinksStackPanel.Children)
             {
-                HyperlinkButton hb = child as HyperlinkButton;
+                var hb = child as HyperlinkButton;
                 if (hb != null && hb.NavigateUri != null)
                 {
-                    if (hb.NavigateUri.ToString().Equals(e.Uri.ToString()))
-                    {
-                        VisualStateManager.GoToState(hb, "ActiveLink", true);
-                    }
-                    else
-                    {
-                        VisualStateManager.GoToState(hb, "InactiveLink", true);
-                    }
+                    VisualStateManager.GoToState(hb,
+                                                 hb.NavigateUri.ToString().Equals(e.Uri.ToString())
+                                                     ? "ActiveLink"
+                                                     : "InactiveLink", true);
                 }
             }
         }
@@ -43,10 +69,16 @@
         /// <summary>
         /// If an error occurs during navigation, show an error window
         /// </summary>
-        private void ContentFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        private void ContentFrameNavigationFailed(object sender, NavigationFailedEventArgs e)
         {
             e.Handled = true;
             ErrorWindow.CreateNew(e.Exception);
+        }
+
+        private void ContentFrameNavigating(object sender, NavigatingCancelEventArgs e)
+        {
+            var estaNaHome = e.Uri == new Uri("/Home", UriKind.Relative);
+            VerificaAutenticacao(estaNaHome);
         }
     }
 }
